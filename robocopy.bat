02cc86be87604ee569351a4fa1b5457e58c6a4f9
Violinist <violinist.david@yahoo.com>
2021-08-04 13:56:53 -0700

    first .bat file

---
+++ robocopy.bat
@@ -0,0 +1,11 @@
+:: The following line is the general syntax of the robocopy command.
+:: robocopy C:\Users\source_dir C:\Users\destination_dir
+
+:: /e recursively copies, including empty dirs.
+:: /b is backup mode, copies all files including those for which you do not have permissions.
+
+:: To Schedule a task:
+:: Open 'Task Scheduler'
+:: 'Create a Basic Task'
+:: Open Properties for the new task
+:: Check box to 'Run with the highest privileges'
