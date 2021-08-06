bbb72d7a7b1a882f3acf1a5e38c93be8ba2a4f00
Violinist <violinist.david@yahoo.com>
2021-08-05 22:22:32 -0700

    Powershell array and foreach loop

--- get_sys.ps1
+++ get_sys.ps1
@@ -5,26 +5,27 @@
 #Get-EventLog -LogName System -InstanceID 16
 #Get-EventLog -LogName System -Newest 20
 
-Get-EventLog -LogName System -Newest 500 | Group-Object -Property Source | Format-Table -AutoSize -Wrap
+#Get-EventLog -LogName System -Newest 500 | Group-Object -Property Source | Format-Table -AutoSize -Wrap
 #Same thing, but using a variable
 #$Events = Get-EventLog -LogName System -Newest 500
 #$Events | Group-Object -Property Source | Format-Table -AutoSize -Wrap
 
 #Portions of commands can be stored as variables then run using Invoke-Expression
-$BaseCmd = "Get-EventLog -LogName System"
-$Last24 = "-After (Get-Date).AddDays(-1) | Out-File -FilePath .\last_24.txt"
+
+$DesktopPath = [Environment]::GetFolderPath("Desktop")
+$ErrPath = Join-Path -Path $DesktopPath -ChildPath errors.txt
+$LastPath = Join-Path -Path $DesktopPath -ChildPath last_24.txt
+$ErrPath
+$LastPath
+$BaseCmd = "Get-EventLog -LogName System "
+$Last24 = "-After (Get-Date).AddDays(-1) | Out-File -FilePath $LastPath"
 $New20 = "-Newest 20"
-$ErrEntry = "-EntryType Error | Out-File -FilePath .\errors.txt"
+$ErrEntry = "-EntryType Error | Out-File -FilePath $ErrPath"
 $Inst16 = "-InstanceID 16"
 $Source500 = "-Newest 500 | Group-Object -Property Source | Format-Table -AutoSize -Wrap"
-Invoke-Expression $BaseCmd$New20
-Invoke-Expression $Var1"-Newest 20"
 
 #Putting the variables in an array then looping through.
-
-$log_cmd=$Last24,$New20,$ErrEntry,$Inst16,Source500
+$log_cmd=$Last24,$New20,$ErrEntry,$Inst16,$Source500
 foreach ($log_req in $log_cmd) {
   Invoke-Expression $BaseCmd$log_req;
  }
-
-$DesktopPath = [Environment]::GetFolderPath("Desktop")
