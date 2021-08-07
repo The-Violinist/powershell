c7fd03dd814267f1905b0a2b8ab26a84a5bd26a5
Violinist <violinist.david@yahoo.com>
2021-08-06 17:17:17 -0700

    Get-Process update

--- sys_info.ps1
+++ sys_info.ps1
@@ -8,16 +8,16 @@
 ### Main ###
 
 # 1. Print to the terminal screen all active processes ordered by highest CPU time consumption at the top.
-Get-Process | Sort-Object CPU -Descending
+# Get-Process | Sort-Object CPU -Descending
 
 # 2. Print to the terminal screen all active processes ordered by highest Process Identification Number at the top.
-Get-Process | Sort-Object id -Descending
+# Get-Process | Sort-Object id -Descending
 
 # 3. Print to the terminal screen the top five active processes ordered by highest Working Set (WS(K)) at the top.
-Get-Process | Sort-Object WorkingSet -Descending | Select-Object -First 5
+# Get-Process | Sort-Object WorkingSet -Descending | Select-Object -First 5
 
 # 4. Start the process Internet Explorer (iexplore.exe) and have it open https://owasp.org/www-project-top-ten/.
-Start-Process -FilePath 'C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe' -ArgumentList 'https://owasp.org/www-project-top-ten/'
+# Start-Process -FilePath 'C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe' -ArgumentList 'https://owasp.org/www-project-top-ten/'
 
 # 5. Start the process Internet Explorer (iexplore.exe) ten times using a for loop. Have each instance open https://owasp.org/www-project-top-ten/.
 $StartEdge = "Start-Process -FilePath 'C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe' -ArgumentList 'https://owasp.org/www-project-top-ten/'"
@@ -28,7 +28,7 @@ for ($i = 1 ; $i -le 10 ; $i++)
     Start-Sleep -s 1
 }
 # 6. Close all Internet Explorer windows.
-Stop-Process -Name "msedge"
+# Stop-Process -Name "msedge"
 
 # 7. Kill a process by its Process Identification Number. Choose a process whose termination won’t destabilize the system, such as Internet Explorer or MS Edge.
 ### FUNCTIONS ###
