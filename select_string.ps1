92dc0b76b6da4f4e8e22002d22133a4154a749a4
Violinist <violinist.david@yahoo.com>
2021-08-10 17:44:03 -0700

    Select-String and regex

---
+++ select_string.ps1
@@ -0,0 +1,19 @@
+﻿### VARIABLES ###
+# Create the file path for the output file
+$DesktopPath = [Environment]::GetFolderPath("Desktop")
+$file_path = Join-Path -Path $DesktopPath -ChildPath network_report.txt
+
+### FUNCTIONS ###
+function print_ips {
+# Create a log of all the network interface information
+ipconfig /all | Out-File -FilePath $file_path
+# Find the address strings from the output file
+$ipconfig = Select-String -Path $file_path -Pattern 'ipv4' -AllMatches
+    foreach ($line in $ipconfig) {
+        if ($line -match "\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b") { 
+        $matches[0]
+}}}
+
+### MAIN ###
+print_ips
+### END ###
