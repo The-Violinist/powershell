bf3de0cc621e35a16525ccdd4170f04668a88c93
Violinist <violinist.david@yahoo.com>
2021-08-17 14:52:51 -0700

    updated robocopy

--- robocopy.bat
+++ robocopy.bat
@@ -1,6 +1,9 @@
 :: The following line is the general syntax of the robocopy command.
 :: robocopy C:\Users\source_dir C:\Users\destination_dir
 
+:: Example backup of a user profile
+:: robocopy <source> <destination> *.* /XO /XJ /COPYALL /E /PURGE /R:0 /NP /W:1
+
 :: /e recursively copies, including empty dirs.
 :: /b is backup mode, copies all files including those for which you do not have permissions.
 
